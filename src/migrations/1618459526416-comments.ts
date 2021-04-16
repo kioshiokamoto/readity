8c5e10975144fcabe031d080e14eaef185f805f2
Kioshi Okamoto <kjor29@gmail.com>
2021-04-15 20:17:26 -0500

    Agregar migracion de comentarios, instalar modulo Cors y preparar servidor para peticiones desde cliente

---
+++ src/migrations/1618459526416-comments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class comments1618459526416 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

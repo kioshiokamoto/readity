2154e1d6812a0de81b6fbcd9dd8d9d66f32dbcce
Kioshi Okamoto <kjor29@gmail.com>
2021-04-16 01:30:43 -0500

    :dart: IMPROVE: crear componente Navbar y agregar en layout

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,26 @@
+import { AppProps } from 'next/app';
+import { Fragment } from 'react';
+import Axios from 'axios';
+import { useRouter } from 'next/router';
+
 import '../styles/globals.css';
 
-import { AppProps } from 'next/app';
-
-import Axios from 'axios';
+import Navbar from '../components/Navbar';
 
 Axios.defaults.baseURL = `http://localhost:5000/api`;
 Axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const { pathname } = useRouter();
+	const authRoutes = ['/login', '/register'];
+	const authRoute = authRoutes.includes(pathname);
+
+	return (
+		<Fragment>
+			{!authRoute && <Navbar />}
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
 export default App;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,34 +1,13 @@
 import Head from 'next/head';
 
-import RedditLogo from '../images/reddit.svg';
-import Link from 'next/link';
+
 export default function Home() {
 	return (
 		<div>
 			<Head>
 				<title>Readity: the front page of the internet</title>
 			</Head>
-			<div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
-				{/* logo and title */}
-				<div className="flex items-center">
-					<Link href="/">
-						<a>
-							<RedditLogo className="w-8 h-8 mr-2" />
-						</a>
-					</Link>
-					<span className="text-2xl font-semibold">
-						<Link href="/">
-							<a>Readity</a>
-						</Link>
-					</span>
-				</div>
-				{/* Search */}
-				<div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
-					<i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
-					<input type="text" className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none" />
-				</div>
-				{/* Auth */}
-			</div>
+			
 		</div>
 	);
 }

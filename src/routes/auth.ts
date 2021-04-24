c32665020153f3c10ea2f1a6015865e2a13b45d4
Kioshi Okamoto <kjor29@gmail.com>
2021-04-24 14:25:04 -0500

    :dart: IMPROVE: Preparar codigo para despliegue

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -68,6 +68,7 @@ const login = async (req: Request, res: Response) => {
 			'Set-Cookie',
 			cookie.serialize('token', token, {
 				httpOnly: true,
+				//Eliminar secure si no cuenta con https
 				secure: process.env.NODE_ENV === 'production',
 				sameSite: 'strict',
 				maxAge: 60 * 60,
@@ -88,6 +89,7 @@ const logout = (_: Request, res: Response) => {
 		'Set-Cookie',
 		cookie.serialize('token', '', {
 			httpOnly: true,
+			//Eliminar secure si no cuenta con https
 			secure: process.env.NODE_ENV === 'production',
 			sameSite: 'strict',
 			expires: new Date(0),
